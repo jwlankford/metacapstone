decdebc689a0a98bcb66435dee36bc40c3d79555
Jeremy Lankford <jwlankford@gmail.com>
2023-02-27 15:39:15 -0500

    update styling.

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<h1> Homepage </h1>
+      <header>Header
+        <nav>
+            <ul className="menu">
+                <li className="menu-item"><a href="/">Home</a></li>
+                <li className="menu-item"><a href="/about">About</a></li> 
+                <li className="menu-item"><a href="/contact">Contact</a></li> 
+            </ul>
+        </nav>
+        </header>
+        <main>Main</main>
+        <footer>Footer</footer>
     </div>
   );
 }
